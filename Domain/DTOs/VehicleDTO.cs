013c781ec93235611927e4b5c2a2a5a9285bff34
lincon <linconsilva08@gmail.com>
2025-11-15 18:16:02 -0300

    post create vehicle

---
+++ Domain/DTOs/VehicleDTO.cs
@@ -0,0 +1,8 @@
+namespace minimals_api.Domain.DTOs;
+
+public record VehicleDTO
+{
+    public string Nome { get; set; } = default!;
+    public string Marca { get; set; } = default!;
+    public int Ano { get; set; } = default!;
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,16 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using minimals_api.Domain.DTOs;
+using minimals_api.Domain.Entities;
 using minimals_api.Domain.Interfaces;
 using minimals_api.Domain.ModelViews;
 using minimals_api.Domain.Services;
 using minimals_api.Infra.Db;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<IAdmService, AdmService>();
+builder.Services.AddScoped<IVehicleService, VehicleService>();
 builder.Services.AddDbContext<DbContexto>(options =>
 {
     options.UseMySql(builder.Configuration.GetConnectionString("mysql"),
@@ -16,18 +19,39 @@ builder.Services.AddDbContext<DbContexto>(options =>
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 var app = builder.Build();
+#endregion
 
+#region Home
 app.MapGet("/", () => TypedResults.Redirect("/swagger"));
+#endregion
 
-app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdmService admService) =>
+#region Adm
+app.MapPost("/adm/login", ([FromBody] LoginDTO loginDTO, IAdmService admService) =>
 {
     if (admService.Login(loginDTO) != null)
         return Results.Ok("Login com sucesso");
     else
         return Results.Unauthorized();
 });
+#endregion
 
+#region Vehicle
+app.MapPost("/vechiles", ([FromBody] VehicleDTO vehicleDto, IVehicleService vehicleService) =>
+{
+    var vehicle = new Vehicle
+    {
+        Nome = vehicleDto.Nome,
+        Marca = vehicleDto.Marca,
+        Ano = vehicleDto.Ano
+    };
+    vehicleService.Add(vehicle);
+    return Results.Created($"/vehicles/{vehicle.Id}", vehicle);
+});
+#endregion
+
+#region App
 app.UseSwagger();
 app.UseSwaggerUI();
 
 app.Run();
+#endregion
